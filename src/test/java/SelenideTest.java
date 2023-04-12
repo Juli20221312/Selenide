c1aad13676cecba6435fd6d0557c19710321bba5
Julia <vondel@mail.ru>
2023-04-12 13:21:48 +0600

    Add in test LocalDate

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,37 +1,37 @@
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SelenideTest {
     @Test
     public void test1() {
+
         open("http://localhost:9999/");
         $("span[data-test-id=city] input").setValue("Москва");
 
-        SelenideElement dateInput = $("span[data-test-id=date] input");
-        if (!dateInput.getValue().isEmpty()) {
-            dateInput.clear();
-        }
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_YEAR, 5);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String date = dateFormat.format(calendar.getTime());
+        String planningDate = generateDate(3);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
 
         $("span[data-test-id=name] input").setValue("Иванов Иван");
         $("span[data-test-id=phone] input").setValue("+79138565456");
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
         $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate),
+                        Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
-
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 }
